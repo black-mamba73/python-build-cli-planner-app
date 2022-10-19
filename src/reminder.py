e46807f1070ad9b270236f70da40a063f35676ca
black-mamba73 <j.n.orihito.111@gmail.com>
2022-10-20 06:18:53 +1000

    Update reminder.py

--- src/reminder.py
+++ src/reminder.py
@@ -5,7 +5,3 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
-
-class PoliteRemenber(PrefixedReminder):
-    def __init(self, text):
-        super().__init__("please")
