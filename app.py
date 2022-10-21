91f22e89ecb60df384fa081024296cc628516c4a
black-mamba73 <j.n.orihito.111@gmail.com>
2022-10-21 17:18:17 +1000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def handle_input():
         reminder = input("What would you like to be reminded about?: ")
         date = input("When is that due?: ")
 
-        add_reminder(reminder, date, PoliteReminder)
+        add_reminder(reminder, date)
         list_reminders()
     else:
         print("Invalid menu option")
